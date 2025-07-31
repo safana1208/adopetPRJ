0fedffd57f96330e3dc0e2d5539e816215292429
safanaa dapur <2004za54@gmail.com>
2025-07-31 09:31:55 +0300

    Removed old server folder from root

--- server/models/adoption.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const adoptionSchema = new mongoose.Schema({
-  month: String,
-  count: Number
-});
-
-module.exports = mongoose.model("Adoption", adoptionSchema);
--- server/models/donation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const donationSchema = new mongoose.Schema({
-  name: String,
-  amount: Number,
-  date: String
-});
-
-module.exports = mongoose.model("Donation", donationSchema);
